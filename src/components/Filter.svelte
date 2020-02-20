901ad0be5e89a9c3a0aceb93ee5a650ce2e33ed5
Javen Nakamoto <javenkn7@gmail.com>
2020-02-20 11:53:04 -0800

    Creates Filter component to filter todos

---
+++ src/components/Filter.svelte
@@ -0,0 +1,42 @@
+<script>
+  export let filter;
+</script>
+
+<style>
+  footer {
+    padding: 20px 20px 10px;
+  }
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  li > a {
+    color: #000;
+    font-size: 1.2rem;
+    text-transform: uppercase;
+  }
+
+  .selected {
+    color: #ff3e00;
+  }
+</style>
+
+<footer>
+  <ul>
+    <li>
+      <a class={filter === 'all' ? 'selected' : ''} href="#/">All</a>
+    </li>
+    <li>
+      <a class={filter === 'active' ? 'selected' : ''} href="#/active">
+        Active
+      </a>
+    </li>
+    <li>
+      <a class={filter === 'completed' ? 'selected' : ''} href="#/completed">
+        Completed
+      </a>
+    </li>
+  </ul>
+</footer>
