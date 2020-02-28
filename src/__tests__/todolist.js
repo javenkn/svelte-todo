227586eaa8d5f4d5b857aa6a6c413607ebf2e398
Javen Nakamoto <javenkn7@gmail.com>
2020-02-27 16:33:09 -0800

    Adds test for removing todos

--- src/__tests__/todolist.js
+++ src/__tests__/todolist.js
@@ -1,5 +1,6 @@
-import { render, fireEvent } from '@testing-library/svelte';
+import { render, fireEvent, getByText } from '@testing-library/svelte';
 import TodoList from '../components/TodoList.svelte';
+import Todo from '../components/Todo.svelte';
 
 test('todo list renders nothing', () => {
   const { getByTestId } = render(TodoList);
@@ -32,4 +33,20 @@ test('can add a new todo', async () => {
 
   expect(list.children).toHaveLength(1);
   getByDisplayValue(/learn svelte/i);
+});
+
+test('can remove a new todo', async () => {
+  const fakeTodos = [
+    { id: 1, done: false, text: 'Learn Svelte' },
+    { id: 2, done: true, text: 'Learn Svelte testing' },
+  ];
+  const { getByTestId, getAllByText } = render(TodoList, { todos: fakeTodos });
+  const list = getByTestId('todo-list');
+  const button = getAllByText('🗑')[0];
+
+  expect(list.children).toHaveLength(2);
+
+  await fireEvent.click(button);
+
+  expect(list.children).toHaveLength(1);
 });
